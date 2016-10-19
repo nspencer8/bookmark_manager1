4059bcdef12fb11c5300ebbed153a689d8fb5ac5
Royston <rlqhenson@hotmail.com>
2016-10-19 16:19:36 +0100

    add link tests

--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,28 @@ require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/links' do
-     @links = Link.all
-     erb :'index'
+  get "/" do
+
+    erb :'index'
   end
+
+  get '/links' do
+    @links = Link.all
+    erb :'link'
+  end
+
+  get "/links/new" do
+
+    erb :'new_link'
+    #redirect "/links"
+  end
+
+  post '/links' do
+    # Link.create(url: params[:new_url], title: params[:new_title])
+     erb :'link'
+  end
+
+
 
   # start the server if ruby file executed directly
 
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,13 +1,39 @@
 require 'spec_helper'
 
-feature 'submit a new link' do
-  scenario 'fill out form and submit link' do
-    Link.create(url: 'http://www.google.com', title: 'Google')
+feature 'can sign in' do
+  scenario 'click Sign In button' do
 
-    visit "/links/new"
-    fill_in "URL", :with => "http://www.google.com"
-    fill_in "Title", :with => "Google"
-    click_button "Submit"
-    expect(page).to have_content('Google')
+    visit "/"
+    click_button "Sign In"
+    click_button "New Link"
+
+    expect(page).to have_content("URL")
+  end
+
+  feature 'link page' do
+    scenario 'page has links' do
+
+      visit "/"
+      click_button "Sign In"
+      Link.create(url: 'http://www.google.com', title: 'Google')
+
+      expect(page).to have_content('Google')
+    end
+  end
+
+  feature 'submit a new link' do
+    scenario 'fill out form and submit link' do
+
+      visit "/"
+      click_button "Sign In"
+      click_button "New Link"
+
+      fill_in "new_url", :with => "http://www.google.com"
+      fill_in "new_title", :with => 'Google'
+      click_button "Submit"
+      Link.create(url: 'http://www.google.com', title: 'Google')
+
+      expect(page).to have_content('Google')
+    end
   end
 end
